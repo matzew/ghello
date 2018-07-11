a2b47f40b4cb74063efbcfb6e65f187b9292023e
Matthias Wessendorf <matzew@apache.org>
2018-07-11 15:35:40 +0200

    Adding post func

--- cmd/server/hello_server.go
+++ cmd/server/hello_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/matzew/ghello/pkg/config"
@@ -11,9 +12,20 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello Golang</h1>")
 }
 
+func postLogger(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Printf("The type is: %T \n", r.Body)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
+
 func main() {
 	config := config.GetConfig()
 
 	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/post", postLogger)
 	http.ListenAndServe(config.ListenAddress, nil)
 }
