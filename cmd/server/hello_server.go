c94bc13a9e0be2b5c8e402e6f71ca01af8ea62e2
Matthias Wessendorf <mwessend@redhat.com>
2022-09-02 15:19:29 +0200

    422 error
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- cmd/server/hello_server.go
+++ cmd/server/hello_server.go
@@ -1,39 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/cloudevents/sdk-go/v01"
-	"github.com/matzew/ghello/pkg/config"
+	"time"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Golang</h1>")
-}
+	// Simulate at least a bit of processing time.
+	time.Sleep(100 * time.Millisecond)
 
-func postLogger(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("kne-foo", "yo...")
+	w.Header().Add("kne-bar", "blup...")
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	//if reqBytes, err := httputil.DumpRequest(r, true); err == nil {
+	log.Print("Got request")
+	w.Write([]byte("Bad error..."))
+	//} else {
+	//	log.Printf("Error dumping the request: %+v :: %+v", err, r)
+	//}
 
-	marshaller := v01.NewDefaultHTTPMarshaller()
-	// req is *http.Request
-	event, err := marshaller.FromRequest(r)
-	if err != nil {
-		panic(err)
-		//panic("Unable to parse event from http Request: " + err.String())
-	}
-
-	val, ok := event.Get("eventType")
-
-	fmt.Printf("eventType: %s \n", val)
-	if ok == false {
-		panic("should have eventtype...")
-	}
 }
 
 func main() {
-	config := config.GetConfig()
 
-	http.HandleFunc("/", helloWorld)
-	http.HandleFunc("/post", postLogger)
-	http.ListenAndServe(config.ListenAddress, nil)
+	m := http.NewServeMux()
+	m.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", m)
 }
